25de516a972c4036ad86399d2a1e98a496691b8a
Reem Youssef <your7095@usherbrooke.ca>
2026-06-20 22:10:19 -0400

    Continued controller move to x
    Started the base idea of the controller, the forme and the content of the matrix Q and R are not completed, not yet tested

--- simulation/controllers/move_to_x.py
+++ simulation/controllers/move_to_x.py
@@ -2,6 +2,8 @@
 #!/usr/bin/env python3
 
 import numpy as np
+from simulation.cart_pole.cartpole import cartpole_symbolic
+from simulation.lib.lqr import compute_lqr_gain
 
 class MoveToPointController:
     @staticmethod
@@ -14,7 +16,19 @@ class MoveToPointController:
         """
         x, dx, theta, dtheta = state
 
-        # TODO : 
-        Tm = 1.0
+        A, b = cartpole_symbolic()
+        Q = np.array() #pas trop sûr du forme, à complèter
+        R = np.array()
 
-        return Tm
+        K = compute_lqr_gain(A, b, Q, R)
+        
+        current_state = np.array[x, dx, theta, dtheta]
+        target_state = np.array[target, 0.0, 0.0, 0.0]
+        error_state = target_state - current_state
+
+
+        u = -MoveToPointController.K @ error_state
+        Tm = float(u.item())
+
+        return Tm #pas encore testé
+    
