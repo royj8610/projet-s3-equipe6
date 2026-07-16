90575aa6dcf1a6dd67a73b605c880ee88b2b0a7a
royj8610 <jean-nyckolas.roy@usherbrooke.ca>
2026-07-16 15:12:51 -0400

    refactoring

--- code-arduino/include/board.h
+++ code-arduino/include/board.h
@@ -1,42 +1,36 @@
 #if defined(BOARD_ESP32)
-    // Includes
-    #include <ESP32Servo.h>
+// Includes
+#include <ESP32Servo.h>
 
-    // Code specific to ESP32 runs here
-    constexpr const char* BOARD_TYPE = "ESP32";
+#define SPI_SCK 18
+#define SPI_MISO 19
+#define SPI_MOSI 23
 
-    #define SPI_SCK 18
-    #define SPI_MISO 19
-    #define SPI_MOSI 23
+#define SERVO_PIN 33
+#define SERVO_OFFSET 0
 
-    #define SERVO_PIN 33
-    #define SERVO_OFFSET 0
+#define AS5047P_CS_PIN 25 // Encoder chip select pin
+#define MCP2515_CS_PIN 32 // CAN module chip select pin
 
-    #define AS5047P_CS_PIN 25 // Encoder chip select pin
-    #define MCP2515_CS_PIN 32 // CAN module chip select pin
-
-    #define INTERRUPT_PIN 0
+#define INTERRUPT_PIN 4
 
 #elif defined(BOARD_MEGA)
-    // Includes
-    #include <Servo.h>
+// Includes
+#include <Servo.h>
 
-    // Code specific to Arduino Mega 2560 runs here
-    constexpr const char* BOARD_TYPE = "Mega2560";
+#define SPI_SCK 52
+#define SPI_MISO 50
+#define SPI_MOSI 51
 
-    #define SPI_SCK 52
-    #define SPI_MISO 50
-    #define SPI_MOSI 51
+#define SERVO_PIN 2
+#define SERVO_OFFSET 0
 
-    #define SERVO_PIN 2
-    #define SERVO_OFFSET 0
+#define AS5047P_CS_PIN 46 // Encoder chip select pin
+#define MCP2515_CS_PIN 47 // CAN module chip select pin
 
-    #define AS5047P_CS_PIN 46 // Encoder chip select pin
-    #define MCP2515_CS_PIN 47 // CAN module chip select pin
-
-    #define INTERRUPT_PIN 21
+#define INTERRUPT_PIN 21
 
 #else
-    // Throws a compilation error if neither board is selected
-    #error "Unsupported hardware target! Please select ESP32 or Arduino Mega 2560."
+// Throws a compilation error if neither board is selected
+#error "Unsupported hardware target! Please select ESP32 or Arduino Mega 2560."
 #endif
