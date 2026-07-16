76a3eb1734a30dcb50d831002dded598d379e9e0
Squynijos <81109473+Squynijos@users.noreply.github.com>
2026-07-16 10:13:13 -0400

    REFACT : Clean up encodeur

--- code-arduino/src/main.cpp
+++ code-arduino/src/main.cpp
@@ -5,17 +5,50 @@
  *
  */
 
+//-----------------------------------------
+//                 IMPORTS
+//-----------------------------------------
 #include <Arduino.h>
-#include <encoder.h>
-#include <encoderAlt.h>
+#include <SPI.h>
+#include <AS5047P.h>
+#include "encoder.h"
 #include "communication_json.h"
 
-unsigned long dernierEnvoi = 0;
+//-----------------------------------------
+//               Constantes
+//-----------------------------------------
 const unsigned long periodeEnvoi = 100; // 100 ms = 10 Hz
+
+//-----------------------------------------
+//                Objects
+//-----------------------------------------
+// CS pin 9, SPI speed default from library header (can pass a custom speed)
+AS5047P as5047p(9);
+
+//-----------------------------------------
+//                Variables
+//-----------------------------------------
+unsigned long dernierEnvoi = 0;
+
+//-----------------------------------------
+//              Main Program
+//-----------------------------------------
 void setup()
 {
-  initialiserCommunicationJson();
+  // Init Serial monitor
   Serial.begin(115200);
+  delay(1000); // Attent un peu pour le start du sérial port
+
+  // Init encoder AS50407
+  Serial.print("> Init AS5047P ");
+  while (!as5047p.initSPI()) {
+    Serial.print(".");
+    delay(500); 
+  }
+  Serial.println(F("> AS5047P sensor successfully initialized."));
+
+  initialiserCommunicationJson();
+  
 }
 
 void loop(){
@@ -26,6 +59,6 @@ void loop(){
     dernierEnvoi = millis();
     envoyerEtatAuRaspberry();
   }
-  
-  Serial.print(encoderAltGetAngle());
+
+  Serial.print(as5047p.readAngleDegree(true));
 }
