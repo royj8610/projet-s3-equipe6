caa9306caeef809f4ef14011bf096a8a06566394
royj8610 <jean-nyckolas.roy@usherbrooke.ca>
2026-07-16 09:35:40 -0400

    Encoder (#41)
    * encoder
    
    * change nom
    
    * en train d'ajouter info
    
    * working on interpretation
    
    * fini encoder, ajouté au main
    
    * resolve implementation into main
    
    * ajouter librarie
    
    * version alternative du code encodeur en utilisant une librarie existante
    
    * finition encoder
    
    ---------
    
    Co-authored-by: Chloe <chloe.hagege@usherbrooke.ca>
    Co-authored-by: Liam Lebrun <81109473+Squynijos@users.noreply.github.com>

--- code-arduino/src/main.cpp
+++ code-arduino/src/main.cpp
@@ -1,18 +1,11 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include <encoder.h>
+#include <encoderAlt.h>
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.print(encoderAltGetAngle());
 }
