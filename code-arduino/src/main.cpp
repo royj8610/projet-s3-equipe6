698f00e72fb08bc34014818e6d89241086482509
Squynijos <81109473+Squynijos@users.noreply.github.com>
2026-07-16 09:49:49 -0400

    REFACT : Cleanup apres pull request

--- code-arduino/src/main.cpp
+++ code-arduino/src/main.cpp
@@ -1,35 +1,31 @@
+/*
+ * Projet S3 - Robot transporteur de sapin - Moitier de robot
+ *
+ * Programme principal pour le Arduino
+ *
+ */
+
 #include <Arduino.h>
 #include <encoder.h>
 #include <encoderAlt.h>
-
-//     imu_init();
-// }
-
-// void loop() {
-//     imu_afficherAcceleration();
-//     delay(300);
-// }
-
-
-#include <Arduino.h>
 #include "communication_json.h"
+
 unsigned long dernierEnvoi = 0;
 const unsigned long periodeEnvoi = 100; // 100 ms = 10 Hz
-void setup() {
-   initialiserCommunicationJson();
+void setup()
+{
+  initialiserCommunicationJson();
   Serial.begin(115200);
 }
-void loop() {
-   lireCommandeDuRaspberry();
-   // Plus tard ici :
-   // lire IMU
-   // lire encodeurs
-   // faire calculs
-   // contrôler moteur
-   if (millis() - dernierEnvoi >= periodeEnvoi) {
-       dernierEnvoi = millis();
-       envoyerEtatAuRaspberry();
-   }
-}
+
+void loop(){
+  lireCommandeDuRaspberry();
+
+  if (millis() - dernierEnvoi >= periodeEnvoi)
+  {
+    dernierEnvoi = millis();
+    envoyerEtatAuRaspberry();
+  }
+  
   Serial.print(encoderAltGetAngle());
 }
