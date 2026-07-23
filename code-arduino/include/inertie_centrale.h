55ae0261882ce0d2092f5b789ef94cc36f5ba17a
royj8610 <jean-nyckolas.roy@usherbrooke.ca>
2026-07-23 13:11:05 -0400

    Comment IMU

--- code-arduino/include/inertie_centrale.h
+++ code-arduino/include/inertie_centrale.h
@@ -1,17 +1,14 @@
-#ifndef INERTIE_CENTRALE_H
-#define INERTIE_CENTRALE_H
+// #ifndef INERTIE_CENTRALE_H
+// #define INERTIE_CENTRALE_H
 
-#include <Arduino.h>
+// #include <Arduino.h>
 
+// void imu_init();
 
-void imu_init();
+// void imu_afficherAcceleration();
 
+// double imu_getAccelX();
+// double imu_getAccelY();
+// double imu_getAccelZ();
 
-void imu_afficherAcceleration();
-
-
-double imu_getAccelX();
-double imu_getAccelY();
-double imu_getAccelZ();
-
-#endif
+// #endif
